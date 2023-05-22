c669b21936b0270428a21d9f756d1e05c04b9602
Sathvik Mulukutla <darthkitten2228@gmail.com>
2023-05-23 01:00:26 +0530

    Removed hello world files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <h1><p>Welcome to the Atlas Game!</p></h1>
+        <gameMode></gameMode>
+        <p>{{ flaskGreeting }}</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    data: function(){
+        return {
+            greeting: 'Hello, Vue!',
+            flaskGreeting: ''
+        }
+    },
+    created: async function(){
+        const gResponse = await fetch("http://localhost:5000/greeting");
+        const gObject = await gResponse.json();
+        this.flaskGreeting = gObject.greeting;
+    }
 }
+
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/gameMode.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id = 'div1'> 
+        <h2>Game Mode</h2>
+        <h3> <form action = "http://localhost:5000/gm" id = "game_mode" name = "game_mode" method = "post">
+            <input type="radio" id="local" name= "mode" value="local">
+            <label for="local">Local</label><br>
+            <input type="radio" id="online" name="mode" value="online">
+            <label for="online">Online</label><br>
+            <button type="submit" form="game_mode" value="Submit">Submit</button>
+            </form></h3>
+        </div> 
+</template>
