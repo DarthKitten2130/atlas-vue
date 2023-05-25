fd6dc36675a722574ee83a2057707a3dac2d1227
Sathvik Mulukutla <darthkitten2228@gmail.com>
2023-05-25 15:42:52 +0530

    Removed Flask API from Home Page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
     <div id="app">
         <h1><p>Welcome to the Atlas Game!</p></h1>
         <gameMode></gameMode>
-        <p>{{ flaskGreeting }}</p>
     </div>
     <div>
         
@@ -22,14 +21,8 @@ export default{
     data: function(){
         return {
             greeting: 'Hello, Vue!',
-            flaskGreeting: ''
         }
     },
-    created: async function(){
-        const gResponse = await fetch("http://localhost:5000/greeting");
-        const gObject = await gResponse.json();
-        this.flaskGreeting = gObject.greeting;
-    }
 }
 
 </script>
