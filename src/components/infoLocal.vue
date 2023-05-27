2f562f45efbab7c4717f732b811c68a9e3c38eb1
Sathvik Mulukutla <darthkitten2228@gmail.com>
2023-05-28 03:06:52 +0530

    Added Count Function

--- src/components/infoLocal.vue
+++ src/components/infoLocal.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
-        <form method="post">
+        <form>
             <label for="playerNum">How Many Players?</label><br>
-            <input id="playerNum" name = "playerNum" v-mode="playerNum"><br>
-            <label for="userName"></label>
+            <input id="playerNum" name = "playerNum" v-model="playerNum"><br>
+            <br>
+            <label for="userName">Username(s) (Seperate multiple usernames with commas)</label><br>
+            <input id="userName" name="playerNum" v-model="userName">
+            <br>
+            <br>
+            <button v-on:click="playerCount">Play!</button>
         </form>
+        <h3>{{ playerNum }}<br>
+            {{ userName.split(',') }}</h3>
     </div>
 </template>
 
@@ -15,6 +22,13 @@
             return{
             playerNum: 0,
             userName : '',
+            }
+        },
+        methods:{
+            playerCount() {
+                if (this.playerNum != this.userName.split(',').length) {
+                alert("Number of Players and Usernames isn't the same!")
+                }
             }
         }
     }
