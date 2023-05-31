fa36bc37633db8b23465d51e21f438f0b95951ef
Sathvik Mulukutla <darthkitten2228@gmail.com>
2023-06-01 00:01:37 +0530

    incorporated infoLocal into App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
     <div id="app">
         <h1><p>Welcome to the Atlas Game!</p></h1>
-        <gameMode></gameMode>
-
-        <infoLocal></infoLocal>
     </div>
+    
+    <div id = 'gameMode'> 
+        <h2>Game Mode</h2>
+        <h3><form id = "game_mode" name = "game_mode" method = "post">
+                <input type="radio" id="local" name= "mode" value="local" v-model="gm">
+                <label for="local">Local</label><br>
+                <input type="radio" id="online" name="mode" value="online" v-model="gm">
+                <label for="online">Online</label><br>
+            </form></h3>
+            <h3>{{ gm }}</h3>
+    </div> 
+
+    <div id="info">
+        <form>
+            <label for="playerNum">How Many Players?</label><br>
+            <input id="playerNum" name="playerNum" v-model="tempPlayerNum"><br>
+            <br>
+            <label for="userName">Usernames (Seperate multiple usernames with commas)</label><br>
+            <input id="userName" name="playerNum" v-model="tempUserNames">
+            <br>
+            <br>
+            <button v-on:click="playerCount">Play!</button>
+        </form>
+        <h3>{{ tempUserNames.split(',') }} <br>
+            {{ tempPlayerNum }} <br>
+            {{ equalUserNames }}
+        </h3>
+
+    </div>
+        <h3>{{equalNames}}</h3>
+        <atlasLocal v-if = "equalNames"></atlasLocal>
     <br>
     <div>
-        <atlasLocal v-if = "equalNames"></atlasLocal>
-        <h3>{{equalNames}}</h3>
+        
     </div>
 </template>
 
 <script>
-import gameMode from './components/gameMode.vue'
-import infoLocal from './components/infoLocal.vue'
 import atlasLocal from './components/atlasLocal.vue'
 
 export default{
 
     name: 'App',
     components: {
-        gameMode,
-        infoLocal,
         atlasLocal
     },
 
@@ -30,15 +53,47 @@ export default{
         
         localStorage.setItem('playerNum',2),
         localStorage.setItem('userNames',''),
-        localStorage.setItem('equalNames',false)
+        localStorage.setItem('equalNames','false'),
+        console.log(localStorage.getItem('equalNames'))
+
     },
 
     data(){
         return {
-            greeting: 'Hello, Vue!',
-            playerNum: localStorage.getItem('playerNum'),
-            userNames: localStorage.getItem('userNames'),
-            equalNames: localStorage.getItem('equalNames')
+            gm: 'local',
+            playerNum: parseInt(localStorage.getItem('playerNum')) || 2,
+            userNames: localStorage.getItem('userNames') || '',
+            equalNames: localStorage.getItem('equalNames') === true,
+            tempPlayerNum: 2,
+            tempUserNames: '',
+        }
+    },
+    computed: {
+
+        equalUserNames() {
+
+            return this.tempPlayerNum == this.tempUserNames.split(',').length ? true : false
+        }
+
+    },
+    methods: {
+        playerCount() {
+            localStorage.setItem('playerNum', this.tempPlayerNum.toString());
+            localStorage.setItem('userNames', this.tempUserNames);
+
+            if (this.tempPlayerNum == 1) {
+                alert("You can't have just one player!");
+            } 
+            
+            else if (this.equalUserNames == false) {
+                alert("Number of Players and Usernames isn't the same!");
+            } 
+            
+            else {
+                localStorage.setItem('equalNames', this.equalUserNames);
+            }
+
+            console.log(this.equalUserNames)
         }
     }
 }
